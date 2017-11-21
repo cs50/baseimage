a1769f4a5a0d33d88826b2938c1baad7eeed6b44
David J. Malan <malan@harvard.edu>
2017-11-21 02:02:03 -0500

    removed aliases

--- etc/profile.d/baseimage.sh
+++ etc/profile.d/baseimage.sh
@@ -1,14 +1,2 @@
-# Aliases
-alias cd="HOME=~/workspace cd"
-alias cp="cp -i"
-alias ll="ls -l --color=auto"
-alias mv="mv -i"
-alias pip="pip3"
-alias python="python3"
-alias rm="rm -i"
-
-# Environment variables
-export EDITOR=nano
-
 # File mode creation mask
 umask 0077
