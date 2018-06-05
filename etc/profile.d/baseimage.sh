c0e525d00c126c61c8a6aac461d8db5c796d41e1
Kareem Zidane <kzidane@cs50.harvard.edu>
2018-06-05 18:19:17 -0400

    added aliases

--- etc/profile.d/baseimage.sh
+++ etc/profile.d/baseimage.sh
@@ -2,3 +2,13 @@
 if [ "$(id -u)" != "0" ]; then
     umask 0077
 fi
+
+# Aliases
+alias cp="cp -i"
+alias ll="ls -l --color=auto"
+alias mv="mv -i"
+alias pip="pip3 --no-cache-dir"
+alias pip3="pip3 --no-cache-dir"
+alias python="python3"
+alias rm="rm -i"
+alias sudo="sudo "
