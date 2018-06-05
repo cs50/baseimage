ede6f7e1ad8182dd37e1cb4996799db2a915cbd0
Kareem Zidane <kzidane@cs50.harvard.edu>
2018-06-05 18:27:36 -0400

    keeping python and pip aliases only

--- etc/profile.d/baseimage.sh
+++ etc/profile.d/baseimage.sh
@@ -4,11 +4,6 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Aliases
-alias cp="cp -i"
-alias ll="ls -l --color=auto"
-alias mv="mv -i"
 alias pip="pip3 --no-cache-dir"
 alias pip3="pip3 --no-cache-dir"
 alias python="python3"
-alias rm="rm -i"
-alias sudo="sudo "
